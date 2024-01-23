cfdced45fa76b823000e75655c617f02221ccaf3
Gregory N <gregoryn@devexpress.com>
2024-01-23 02:34:14 -0800

    add product page

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -11,7 +11,7 @@ const ProductCard = (props: ProductCardProps) => {
     <div className="justify-center items-center shadow-md bg-[#141e28] rounded-lg p-4 m-4">
       <h2 className="text-xl font-bold text-center pb-4">{props.name}</h2>
       <div className="flex justify-center items-center">
-        <img src={props.imageURL} alt={props.name} className="rounded-md w-[90%]" />
+        <img src={props.imageURL} className="rounded-md w-[90%]" />
       </div>
       <p className="p-4 text-lg text-left">{props.description}</p>
       <Button
