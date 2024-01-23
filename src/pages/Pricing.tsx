d4090e4c876b78d8b716745ec0d5002af53453db
Gregory N <gregoryn@devexpress.com>
2024-01-23 03:13:53 -0800

    update pricing page

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -5,7 +5,7 @@ const Pricing = () => {
     <>
       <Navbar />
       <div className='flex min-h-[85vh] m-2 md:m-4 justify-center bg-[#16232e]'>
-        <div className="pb-16 pt-16 text-center md:w-1/2">
+        <div className="pb-16 pt-16 text-center">
           <h1 className="text-4xl font-bold">Pricing</h1>
         </div>
       </div >
