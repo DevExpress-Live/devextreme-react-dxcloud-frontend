5823edcda34e8bd56798fa3bcc92c08275c68827
Gregory N <gregoryn@devexpress.com>
2024-01-23 03:09:26 -0800

    update pricing page

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -4,9 +4,11 @@ const Pricing = () => {
   return (
     <>
       <Navbar />
-      <div>
-        <h1>Pricing</h1>
-      </div>
+      <div className='flex min-h-[85vh] m-2 md:m-4 justify-center bg-[#16232e]'>
+        <div className="pb-16 pt-16 text-center md:w-1/2">
+          <h1 className="text-4xl font-bold">Pricing</h1>
+        </div>
+      </div >
     </>
   );
 };
