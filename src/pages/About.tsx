19f5633b225d8a14a57e424f0ac87f663c2add7f
Gregory N <gregoryn@devexpress.com>
2024-01-30 14:51:01 -0800

    update content

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -8,16 +8,16 @@ const About = () => {
         <div className="pb-16 pt-16 text-center md:w-1/2">
           <h1 className="text-4xl font-bold">About Us</h1>
           <p className="text-xl leading-relaxed mt-4">
-            Welcome to Your Cloud Platform, where innovation meets reliability in the digital realm. We are a team of passionate individuals committed to revolutionizing the way you experience cloud services. Our journey began in [Year], and since then, we have been dedicated to providing cutting-edge cloud solutions, including Cloud Storage, Virtual Private Servers (VPS), and Website Hosting.
+            Welcome to Dx Cloud, where innovation meets reliability in the digital realm. We are a team of passionate individuals committed to revolutionizing the way you experience cloud services. Our journey began in 1998, and since then, we have been dedicated to providing cutting-edge cloud solutions, including Cloud Storage, Virtual Private Servers (VPS), and Website Hosting.
           </p>
           <p className="text-xl leading-relaxed mt-4">
-            At Your Cloud Platform, we believe in the power of simplicity, scalability, and security. Our mission is to empower you with the tools and resources needed to thrive in the ever-evolving digital landscape. Whether you are an individual looking for secure and flexible cloud storage, a developer in need of high-performance virtual servers, or a business aiming to establish a robust online presence through website hosting – we've got you covered.
+            At Dx Cloud, we believe in the power of simplicity, scalability, and security. Our mission is to empower you with the tools and resources needed to thrive in the ever-evolving digital landscape. Whether you are an individual looking for secure and flexible cloud storage, a developer in need of high-performance virtual servers, or a business aiming to establish a robust online presence through website hosting – we've got you covered.
           </p>
           <p className="text-xl leading-relaxed mt-4">
             What sets us apart is our unwavering commitment to customer satisfaction. We understand that your success is our success, and that's why we prioritize reliability, security, and innovation in everything we do. Our team of experts is continuously working to enhance and expand our services to meet the dynamic needs of our users.
           </p>
           <p className="text-xl leading-relaxed mt-4">
-            Thank you for choosing Your Cloud Platform. We look forward to being your trusted partner on your journey to digital excellence.
+            Thank you for choosing Dx Cloud. We look forward to being your trusted partner on your journey to digital excellence.
           </p>
         </div>
       </div >
--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -8,16 +8,19 @@ type PricingData = {
 
 const data: PricingData[] = [
   {
-    title: "Cloud Hosting",
-    content: "Basic",
+    title: "Cloud Storage",
+    content: "Price per 100gb",
+    price: 10,
   },
   {
     title: "Virtual Machines",
-    content: "Standard",
+    content: "Price per CPU core",
+    price: 5,
   },
   {
     title: "Web Hosting",
-    content: "Premium",
+    content: "Price per website",
+    price: 10,
   },
 ];
 
@@ -27,10 +30,10 @@ const Pricing = () => {
       <Navbar />
       <div className='flex min-h-[85vh] m-2 md:m-4 justify-center bg-[#16232e]'>
         <div className="pb-16 pt-16 text-center w-full">
-          <h1 className="text-4xl font-bold">Pricing</h1>
-          <div className="flex items-center justify-center">
+          <h1 className="text-4xl font-bold mb-4">Pricing Monthly</h1>
+          <div className="flex items-center justify-center bg-[#141e28] m-4 pb-4">
             <Accordion
-              className="mt-4 h-96 w-1/2"
+              className="mt-4 h-96 w-1/2 p-4"
               dataSource={data}
               collapsible={true}
               multiple={true}
@@ -54,8 +57,8 @@ const CustomTitle = (data: PricingData) => {
 
 const CustomItem = (data: PricingData) => {
   return (
-    <div className="item">
-      {data.content}
+    <div className="text-xl p-4">
+      {data.content}{": $"}{data.price}
     </div>
   )
 }
