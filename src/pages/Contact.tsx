b6cf566697584d5400b3711346f24942d9986f56
Gregory N <gregoryn@devexpress.com>
2024-01-30 15:30:53 -0800

    update contact page and form

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,4 +1,5 @@
 import Navbar from "../components/navbar/Navbar";
+import ContactForm from "../components/ContactForm";
 
 const Contact = () => {
   return (
@@ -6,7 +7,7 @@ const Contact = () => {
       <Navbar />
       <div className='flex min-h-[85vh] m-2 md:m-4 justify-center bg-[#16232e]'>
         <div className="pb-16 pt-16 text-center md:w-1/2">
-          <h1 className="text-4xl font-bold">Contact Us</h1>
+          <ContactForm />
         </div>
       </div >
     </>
