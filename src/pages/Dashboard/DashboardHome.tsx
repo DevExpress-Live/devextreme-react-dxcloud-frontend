9ff8f6927497f3fc15bcb5bbde1c6dac3389fbd8
Gregory N <gregoryn@devexpress.com>
2024-02-05 19:50:27 -0800

    update dashboard home: add chart

--- src/pages/Dashboard/DashboardHome.tsx
+++ src/pages/Dashboard/DashboardHome.tsx
@@ -1,8 +1,37 @@
+import { useState, useEffect } from "react";
+import TotalNodesChart from "../../components/Dashboard/TotalNodesChart";
+
+type TotalNodes = {
+  type: string;
+  total: number;
+};
 
 const DashboardHome = () => {
+  const [totalNodes, setTotalNodes] = useState<Array<TotalNodes>>([{ type: "", total: 0 }]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch("http://172.22.5.80:8080/dashboard", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          }
+        });
+        const data = await response.json();
+        setTotalNodes(data.totalNodes);
+        console.log(data.totalNodes);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getData();
+  }, []);
+
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h1 className="p-4">Dashboard</h1>
+      <TotalNodesChart totalNodes={totalNodes} />
     </div>
   );
 }
